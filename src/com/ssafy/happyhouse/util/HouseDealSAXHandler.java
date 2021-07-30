4363d4ea29e85e98a284fa13ce65fe1d808ee718
이정훈 <person00333@gmail.com>
2021-07-30 14:38:18 +0900

    APTRent, HouseDeal modify

--- src/com/ssafy/happyhouse/util/HouseDealSAXHandler.java
+++ src/com/ssafy/happyhouse/util/HouseDealSAXHandler.java
@@ -14,11 +14,58 @@ import com.ssafy.happyhouse.model.dto.HouseDeal;
 public class HouseDealSAXHandler extends DefaultHandler {
 	// complete code #08
 	// APTDealSAXHandler Class 를 참조하여, 주택 거래 정보에 맞도록 전체 코드를 작성하세요. 단, 아래 코드를 이용하여 완성하세요.
-	public void startElement(String uri, String localName, String qName, Attributes att ){}
-	public void endElement(String uri, String localName, String qName){}
-	public void characters(char[]ch, int start, int length){}
-	public List<HouseDeal> getHouses() { return null; }
-	public void setHouses(List<HouseDeal> houses) {}
+	
+	/**아파트 거래 정보를 담는다 */
+	private List<HouseDeal> houses;
+	/**파상힌 아파트 거래 정보*/
+	private HouseDeal house;
+	/**태그 바디 정보를 임시로 저장*/
+	private String temp;
+	
+	public HouseDealSAXHandler() {
+		houses = new LinkedList<HouseDeal>();
+	}
+	public void startElement(String uri, String localName, String qName, Attributes att ){
+		if(qName.equals("item")){
+			house = new HouseDeal(HouseSaxParser.no++);
+			house.setType(HouseDeal.HOUSE_DEAL);
+			houses.add(house);
+		}
+	}
+	public void endElement(String uri, String localName, String qName){
+		if(qName.equals("지역코드")) { 
+			house.setCode(Integer.parseInt(temp));
+		}else if(qName.equals("연립다세대")) { 
+			house.setAptName(temp.trim());
+		}else if(qName.equals("법정동")) { 
+			house.setDong(temp.trim());
+		}else if(qName.equals("거래금액")) { 
+			house.setDealAmount(temp);
+		}else if(qName.equals("건축년도")) { 
+			house.setBuildYear(Integer.parseInt(temp));
+		}else if(qName.equals("년")) { 
+			house.setDealYear(Integer.parseInt(temp));
+		}else if(qName.equals("월")) { 
+			house.setDealMonth(Integer.parseInt(temp));
+		}else if(qName.equals("일")) { 
+			house.setDealDay(Integer.parseInt(temp));
+		}else if(qName.equals("전용면적")) { 
+			house.setArea(Double.parseDouble(temp));
+		}else if(qName.equals("지번")) { 
+			house.setJibun(temp);
+		}else if(qName.equals("층")) { 
+			house.setFloor(Integer.parseInt(temp));
+		}
+	}
+	public void characters(char[]ch, int start, int length){
+		temp = new String(ch, start, length);
+	}
+	public List<HouseDeal> getHouses() { 
+		return houses;
+	}
+	public void setHouses(List<HouseDeal> houses) {
+		this.houses = houses;
+	}
 }
 
 
