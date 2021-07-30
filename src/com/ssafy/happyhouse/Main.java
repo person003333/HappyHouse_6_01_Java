61893ee95d49ed81dd43be3d5c4cc2b5a02986d2
김다빈 <ekqls6876@naver.com>
2021-07-30 14:16:52 +0900

    modify Main

--- src/com/ssafy/happyhouse/Main.java
+++ src/com/ssafy/happyhouse/Main.java
@@ -3,7 +3,7 @@ package com.ssafy.happyhouse;
 import com.ssafy.happyhouse.view.HouseInfoView;
 
 public class Main {
-	// 시작!! 
+	// 시작!! 22
 	public static void main(String[] args) {
 		new HouseInfoView();
 	}
